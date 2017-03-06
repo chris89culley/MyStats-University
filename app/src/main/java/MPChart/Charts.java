bac973f621ed68045a0f7a90dfd843a2d0436abe
Jack <jacklundy@hotmail.co.uk>
2017-03-06 21:47:05 +0000

    MPChart has been added to the project

--- app/src/main/java/MPChart/Charts.java
+++ app/src/main/java/MPChart/Charts.java
@@ -1,8 +1,43 @@
 package MPChart;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
 /**
  * Created by Jack on 06/03/2017.
  */
 
-public class Charts {
+public class Charts{
+    /*Takes in Data for the chart, the labels for each bar, a chart object from the view and title of the chart */
+        public static BarData constructBarChart(String[] tags,float[] data, BarChart chart, String DataTitle){
+            ArrayList<BarEntry> entries = new ArrayList<>();
+            ArrayList<String> labels = new ArrayList<>();
+
+            for (int i = 0; i < data.length; i++){
+                entries.add(new BarEntry(i,data[i]));
+                labels.add(tags[i]);
+            }
+
+            BarDataSet barDataSet = new BarDataSet(entries,DataTitle); //Creating a dataSet for the chart
+            chart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));//Setting the X axis labels
+            BarData theData = new BarData(barDataSet);
+            return theData;
+        }
+    /*Overrloading for optional arguements - Chart is labelled Data by default*/
+    public static BarData constructBarChart(String[] tags,float[] data, BarChart chart){
+           return constructBarChart(tags,data, chart ,"Data");
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
@@ -1,20 +1,29 @@
 package com.example.chris.mystats_univeristy;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.github.mikephil.charting.*;
+import com.github.mikephil.charting.charts.BarChart;
+import MPChart.Charts;
 
 import java.util.Iterator;
 
 import Data.CourseTypes;
 import Data.DatabaseInformationQuerier;
+import MPChart.Charts;
 
 ///i am a commetn
 public class HomePage extends AppCompatActivity {
@@ -30,7 +39,30 @@ public class HomePage extends AppCompatActivity {
         //This is an example of how to get all courses by course name and course type
         databaseInfomationQuerier.getAllCoursesByCourseName("Computer Science", CourseTypes.FULL_TIME);
 
+        //New chart objexct being linked too the view
+        BarChart chart = (BarChart) findViewById(R.id.chart);
+
+        //Data to be used by the chart this is just an example
+        float[] data = new float[4];
+        data[0] = 4.0f;
+        data[1] = 1.0f;
+        data[2] = 2.0f;
+        data[3] = 4.0f;
+        String[] labels = new String[4];
+        labels[0] = "One";
+        labels[1] = "Two";
+        labels[2] = "Three";
+        labels[3] = "Four";
+
+        //Using the returned values from the generic function to fill the chart
+        chart.setData(Charts.constructBarChart(labels,data,chart));
+        //Altering chart settings - not necessary
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
 
 
     }
+
 }
