0cd93f66814ccdcddecdfb1f53f64555578bb2d3
Chris <iamchris88@live.co.uk>
2017-03-06 05:37:01 +0000

    Removed dummy method

---
+++ app/src/main/java/Data/DatabaseInformationQuerier.java
@@ -0,0 +1,18 @@
+package Data;
+
+import com.google.firebase.database.DatabaseReference;
+
+/**
+ * Created by chris on 06/03/17.
+ */
+
+public class DatabaseInformationQuerier {
+
+    DatabaseReference database;
+    public DatabaseInformationQuerier(DatabaseReference database){
+        this.database = database;
+    }
+
+
+
+}
