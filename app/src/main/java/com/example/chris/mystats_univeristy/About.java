c3e8e59cae65edfdc80b896ae57931a854fc594e
Daniel Collings <collings8915@gmail.com>
2017-03-07 09:36:40 +0000

    UI menu navigation started.

---
+++ app/src/main/java/com/example/chris/mystats_univeristy/About.java
@@ -0,0 +1,38 @@
+package com.example.chris.mystats_univeristy;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+public class About extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.about_page);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.closed);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
--- app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
@@ -1,8 +1,13 @@
 package com.example.chris.mystats_univeristy;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,36 +22,54 @@ import java.util.StringTokenizer;
 
 public class HomePage extends AppCompatActivity {
 
-    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
-// I am a dummy method to demonstrate a basic grabbing of information from the database :)
-    private void dummyMethod(){
-
-        Log.d("dummy method ", "I am a dummy method and I grab information about a course");
-        database.child("fulltimecourses").child("VV43").addListenerForSingleValueEvent(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        Iterator<DataSnapshot> obs = dataSnapshot.getChildren().iterator();
-                        while(obs.hasNext()){
-                            Log.d("From the datbase" , obs.next().toString());
-                        }
-                        Log.d("Dummy method ", "I am now leaving the dummy method");
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                }
-        );
-    }
+    //    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+//
+//// I am a dummy method to demonstrate a basic grabbing of information from the database :)
+//    private void dummyMethod(){
+//
+//        Log.d("dummy method ", "I am a dummy method and I grab information about a course");
+//        database.child("fulltimecourses").child("VV43").addListenerForSingleValueEvent(
+//                new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        Iterator<DataSnapshot> obs = dataSnapshot.getChildren().iterator();
+//                        while(obs.hasNext()){
+//                            Log.d("From the datbase" , obs.next().toString());
+//                        }
+//                        Log.d("Dummy method ", "I am now leaving the dummy method");
+//                    }
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                }
+//        );
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        dummyMethod();
+//        dummyMethod();
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_home_page);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.closed);
 
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
