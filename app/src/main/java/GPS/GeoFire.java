82077e3146d7b84ceb34745fb05a91952afca635
Teza1532 <Terence.lawson1532@gmail.com>
2017-03-08 19:17:44 +0000

    LocListener for finding current location

---
+++ app/src/main/java/GPS/GeoFire.java
@@ -0,0 +1,17 @@
+package GPS;
+
+/**
+ * Created by Terence Lawson on 08/03/2017.
+ */
+
+public class GeoFire {
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference("path/to/geofire");
+    GeoFire geoFire;
+
+    GeoFire(DatabaseReference database){
+      Final  geoFire = new GeoFire(this.database);
+
+    }
+
+
+}
