5be9e9bb5b4b2e2e2f8a91a416bdec97f64d9e20
Chris <iamchris88@live.co.uk>
2017-03-14 08:11:01 +0000

    Fixed error

--- app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import Data.CourseTypes;
 import Data.DatabaseInformationQuerier;
