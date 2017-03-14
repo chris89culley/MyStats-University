ea07de7e6b0517a4fce4ffe1213eea38f30a3ed9
p4183397 <p4183397@live.tees.ac.uk>
2017-03-14 13:51:40 +0000

    Fixed revert

--- app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
@@ -1,10 +1,16 @@
 package com.example.chris.mystats_univeristy;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,13 +41,15 @@ public class HomePage extends MenuViewActivity  {
     private MenuViewActivity current = this;
     private SeekBar radiusBar;
     private TextView radiusDisplay;
+    private double longitude;
+    private double latitude;
     int sizeOfRadius = 0;
     DatabaseReference database = FirebaseDatabase.getInstance().getReference();
 
     private void updateRadius(int progress){
-        sizeOfRadius = progress*10;
+        sizeOfRadius = progress*5;
         radiusDisplay.setText(String.valueOf(sizeOfRadius) + " km");
-        Log.d("The current size of the rad is " , String.valueOf(sizeOfRadius));
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,22 +93,24 @@ public class HomePage extends MenuViewActivity  {
             public void onClick(View view) {
                 int selectedid = courseTypeButton.getCheckedRadioButtonId();
                 RadioButton button = (RadioButton) (findViewById(selectedid));
-
                 CourseTypes courseType = button.getText().equals("full time courses") ? CourseTypes.FULL_TIME : CourseTypes.PART_TIME;
-                Log.d("cours type ", String.valueOf(courseTypeButton.getCheckedRadioButtonId()));
+
                 //Create the intent to start another activity
                 Intent intent = new Intent(view.getContext(), SearchResults.class);
                 databaseInfomationQuerier.setIntent(intent);
                 databaseInfomationQuerier.setCurrent(current);
-                List<Address> addresses = new ArrayList<>();
+                if(!searchedLocation.getText().equals("My location")){
                 try {
-                    addresses = loc.getFromLocationName(searchedLocation.getText().toString(), 1);
+                    List<Address> addresses = loc.getFromLocationName(searchedLocation.getText().toString(), 1);
+                    longitude = addresses.get(0).getLongitude();
+                    latitude = addresses.get(0).getLatitude();
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
-                if(addresses.size() >0){
-                    RadiusChecker.getHitsAroundLocation(100, addresses.get(0).getLongitude()
-                                                            , addresses.get(0).getLatitude()
+                }}
+                if(longitude != 0.0 && latitude != 0.0){
+                    Log.d(String.valueOf(longitude) , String.valueOf(latitude));
+                    RadiusChecker.getHitsAroundLocation(sizeOfRadius, longitude
+                                                            , latitude
                                                             , searchedCourse.getText().toString(),
                                                             databaseInfomationQuerier,courseType);
                 }
@@ -115,5 +125,30 @@ public class HomePage extends MenuViewActivity  {
 
 
     }
+
+
+    public void areaSearch(){
+// Or, use GPS location data:
+// String locationProvider = LocationManager.GPS_PROVIDER;
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            double latitude=location.getLatitude();
+            double  longitude=location.getLongitude();
+        }
+    }
+
 
 }
