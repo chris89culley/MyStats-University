515e51e3d7aea8b52ed1e63e4474acdbc1dea33c
Chris <iamchris88@live.co.uk>
2017-03-14 06:30:08 +0000

    Geolocation search added but having trouble with connecting

--- app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/HomePage.java
@@ -2,6 +2,8 @@ package com.example.chris.mystats_univeristy;
 
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Context;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,8 +21,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 import Data.Course;
 import Data.CourseTypes;
@@ -31,6 +37,7 @@ public class HomePage extends MenuViewActivity  {
 
     private Button sim;
     private EditText searchedCourse;
+    private EditText searchedLocation;
     private MenuViewActivity current = this;
 
     DatabaseReference database = FirebaseDatabase.getInstance().getReference();
@@ -45,9 +52,21 @@ public class HomePage extends MenuViewActivity  {
 
          final DatabaseInformationQuerier databaseInfomationQuerier = new DatabaseInformationQuerier(database);
 
-
         sim = (Button) findViewById(R.id.simSearch);
-        searchedCourse = (EditText) findViewById(R.id.editText);
+        searchedCourse = (EditText) findViewById(R.id.courseNameEntered);
+        Geocoder test = new Geocoder(this, Locale.ENGLISH);
+        Log.d("working " , String.valueOf(Geocoder.isPresent()));
+        Log.d("tseting " , String.valueOf(test.isPresent()));
+        List<Address> addresses = new ArrayList<>();
+        try {
+            while(addresses.size() == 0){
+                addresses = test.getFromLocation(54.569771 , -1.235611 , 1);
+            }
+            Log.d("i am a test", addresses.get(0).toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         sim.setOnClickListener(new View.OnClickListener(){
             @Override
