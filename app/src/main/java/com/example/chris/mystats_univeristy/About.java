2005f891beab34e8a64f8e6b9b637efb18151e8b
Teza1532 <Terence.lawson1532@gmail.com>
2017-04-07 12:33:58 +0100

    About page layout started background colour changed and the title set up

--- app/src/main/java/com/example/chris/mystats_univeristy/About.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/About.java
@@ -1,20 +1,27 @@
 package com.example.chris.mystats_univeristy;
 
-import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class About extends MenuViewActivity {
-
+    private View view;
+    private TextView pageTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+
+
+        Typeface retroFont = Typeface.createFromAsset(this.getAssets(), "fonts/Market_Deco.ttf");
+
+
+
+        pageTitle = (TextView) findViewById(R.id.title);
+        pageTitle.setTypeface(retroFont);
     }
 
 
