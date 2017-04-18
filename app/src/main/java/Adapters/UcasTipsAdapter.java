1e8fb94fa744c62132a68bccafad8b1ad3d3c236
Teza1532 <Terence.lawson1532@gmail.com>
2017-04-18 19:05:44 +0100

    Set up the fragments on the UCAS Tips Website

---
+++ app/src/main/java/Adapters/UcasTipsAdapter.java
@@ -0,0 +1,42 @@
+package Adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import AboutFragments.AboutFragmentSelecter;
+import UcasTips.UcasTips;
+
+/**
+ * Created by Terence Lawson on 18/04/2017.
+ */
+
+public class UcasTipsAdapter extends FragmentPagerAdapter{
+    private String[] title = new String[]{"Intro","Getting Started","Personal Statement", };
+    Context context;
+    private int pageCount = 3;
+
+    public UcasTipsAdapter(FragmentManager fm, UcasTips ucasTips) {
+        super(fm);
+    }
+    /**
+     * sends  position to the fragment selector.
+     * @param position
+     * @return
+     */
+    @Override
+    public Fragment getItem(int position) {
+        AboutFragmentSelecter frag = new AboutFragmentSelecter(position);
+        return frag;    }
+
+    @Override
+    public int getCount() {
+        return pageCount;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int pos){
+        return title[pos];
+    }}
+
---
+++ app/src/main/java/UcasTips/UcasTipsFragmentSelecter.java
@@ -0,0 +1,10 @@
+package UcasTips;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Terence Lawson on 18/04/2017.
+ */
+
+public class UcasTipsFragmentSelecter extends Fragment {
+}
