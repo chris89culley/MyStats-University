aa93a0c55f9eb777525c3dc3f15da46c24749b9a
Daniel Collings <collings8915@gmail.com>
2017-04-26 12:13:37 +0100

    App font size, colour, line spacing change throughout app to be consistent. - Plus update on entry

--- app/src/main/java/FragmentSelectors/UcasTipsFragmentSelecter.java
+++ app/src/main/java/FragmentSelectors/UcasTipsFragmentSelecter.java
@@ -47,18 +47,14 @@ public class UcasTipsFragmentSelecter extends Fragment {
         try {
             switch (pos){
                 case 0:
-                    view = inflater.inflate(R.layout.ucas_tips_fragment_intro, container, false);
-                    editUcasTipsFragmentIntro();
-                    return view;
-                case 1:
                     view = inflater.inflate(R.layout.ucas_tips_fragment_choosing_the_right_course, container, false);
                     editUcasTipsFragmentChoosingTheRightCourse();
                     return view;
-                case 2:
+                case 1:
                     view = inflater.inflate(R.layout.ucas_tips_fragment_personal_statement, container, false);
                     editUcasTipsFragmentPersonalStatement();
                     return view;
-                case 3:
+                case 2:
                     view = inflater.inflate(R.layout.ucas_tips_fragment_interview, container, false);
                     editUcasTipsFragmentInterview();
                     return view;
@@ -175,11 +171,5 @@ public class UcasTipsFragmentSelecter extends Fragment {
 
     }
 
-    /**
-     * Sets the font type of the Intro Fragment
-     */
-    private void editUcasTipsFragmentIntro() {
-        TextView introText = (TextView) view.findViewById(R.id.ucastips_introduction);
-        introText.setTypeface(retroFont);
-    }
+
 }
