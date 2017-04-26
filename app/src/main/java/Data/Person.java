e5b6f754ce2a5dbbe04e70090a66b15ee0a7bb03
Chris <iamchris88@live.co.uk>
2017-04-26 19:29:10 +0100

    Created a carousel spinner for the about us page with an adapter

---
+++ app/src/main/java/Data/Person.java
@@ -0,0 +1,16 @@
+package Data;
+
+/**
+ * Created by chris on 26/04/17.
+ */
+
+public class Person {
+
+    public int imageResId;
+    public int titleResId;
+
+    public Person(int imageResId, int titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+}
--- app/src/main/java/FragmentSelectors/AboutFragmentSelecter.java
+++ app/src/main/java/FragmentSelectors/AboutFragmentSelecter.java
@@ -1,5 +1,8 @@
 package FragmentSelectors;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,10 +11,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.example.chris.mystats_univeristy.R;
+import com.ogaclejapan.arclayout.ArcLayout;
+
+import java.util.ArrayList;
+
+import Adapters.CoverFlowAdapter;
+import Animations.AnimatorUtils;
+import Data.Person;
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
 import static com.example.chris.mystats_univeristy.R.string.copy_page_intro;
 
@@ -23,8 +43,9 @@ public class AboutFragmentSelecter extends Fragment {
 
         private View view;
         private int pos;
-        Typeface retroFont;
-
+        private Typeface retroFont;
+        private ArcLayout arcLayout;
+    private FeatureCoverFlow mCoverFlow;
 
         /**
          * sets the position of the fragment pager and the course data
@@ -114,22 +135,139 @@ public class AboutFragmentSelecter extends Fragment {
         TextView dlhe = (TextView) view.findViewById((R.id.dlhe));
         dlhe.setTypeface(retroFont);
     }
+
+    private  void animateToLocation(final Button object, final float right, final float down){
+
+        final float startX = object.getX();
+        final float startY = object.getY();
+        Point center = arcLayout.getOrigin();
+
+        TranslateAnimation anim = new TranslateAnimation(0, right, 0 , down );
+        anim.setDuration(1000);
+        anim.setAnimationListener(new TranslateAnimation.AnimationListener(){
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                object.setX(startX + right);
+                object.setY(startY + down);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        object.startAnimation(anim);
+
+
+    }
+    /**
+     * Animates the search arm by rotating it slightly and translating across a bit of the x axis
+     */
+    private void animateToCenter(final Button object){
+
+        final float startX = object.getX();
+        final float startY = object.getY();
+        Point center = arcLayout.getOrigin();
+
+        final float right = center.x - startX - 100;
+        final float down = center.y - startY - 200;
+        TranslateAnimation anim = new TranslateAnimation(0, right, 0 , down );
+        anim.setDuration(1000);
+        anim.setAnimationListener(new TranslateAnimation.AnimationListener(){
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                object.setX(startX + right);
+                object.setY(startY + down);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        object.startAnimation(anim);
+    }
+
+
     /**
      * Sets the font type of the Meet the team Fragment
      */
     private void editCreatersFragment(){
+        mCoverFlow = (FeatureCoverFlow) view.findViewById(R.id.coverflow);
+        final ArrayList<Person> team = new ArrayList<>();
 
+        team.add(new Person(R.drawable.chris, R.string.chris_personal));
+        team.add(new Person(R.drawable.jack, R.string.jack_personal));
+        team.add(new Person(R.drawable.tel, R.string.tel_personal));
+        team.add(new Person(R.drawable.dan, R.string.jack_personal));
+        CoverFlowAdapter mAdapter = new CoverFlowAdapter(this.getContext());
+        mAdapter.setData(team);
+        mCoverFlow.setAdapter(mAdapter);
+        final TextSwitcher mTitle = (TextSwitcher) view.findViewById(R.id.title);
+        Animation in = AnimationUtils.loadAnimation(this.getContext(), R.anim.slide_in_top);
+        Animation out = AnimationUtils.loadAnimation(this.getContext(), R.anim.slide_out_bottom);
+        mTitle.setInAnimation(in);
+        mTitle.setOutAnimation(out);
+
+        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(AboutFragmentSelecter.this.getContext());
+                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+                return textView;
+            }
+        });
+
+        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(AboutFragmentSelecter.this.getContext(),
+                        getResources().getString(team.get(position).titleResId),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                mTitle.setText(getResources().getString(team.get(position).titleResId));
+            }
+
+            @Override
+            public void onScrolling() {
+                mTitle.setText("");
+            }
+        });
+
+
+        /**
+        arcLayout = (ArcLayout) view.findViewById(R.id.arcAboutTeamPage);
         final TextView personDescription = (TextView) view.findViewById(R.id.personDescriptionAboutUs);
 
-        Button chrisButton = (Button) view.findViewById(R.id.chrisButton);
-        Button danButton = (Button) view.findViewById(R.id.danButton);
-        Button jackButton = (Button) view.findViewById(R.id.jackButton);
-        Button telButton = (Button) view.findViewById(R.id.telButton);
+        final Button chrisButton = (Button) view.findViewById(R.id.chrisButton);
+        final Button danButton = (Button) view.findViewById(R.id.danButton);
+        final Button jackButton = (Button) view.findViewById(R.id.jackButton);
+        final Button telButton = (Button) view.findViewById(R.id.telButton);
         chrisButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 personDescription.setText(getString(R.string.chris_personal));
-
+                animateToCenter(chrisButton);
             }
         });
 
@@ -137,6 +275,8 @@ public class AboutFragmentSelecter extends Fragment {
             @Override
             public void onClick(View v) {
                 personDescription.setText(getString(R.string.dan_personal));
+                animateToCenter(danButton);
+
 
             }
         });
@@ -145,6 +285,7 @@ public class AboutFragmentSelecter extends Fragment {
             @Override
             public void onClick(View v) {
                 personDescription.setText(getString(R.string.jack_personal));
+                animateToCenter(jackButton);
 
             }
         });
@@ -153,6 +294,7 @@ public class AboutFragmentSelecter extends Fragment {
             @Override
             public void onClick(View v) {
                 personDescription.setText(getString(R.string.tel_personal));
+                animateToCenter(telButton);
 
             }
         });
@@ -169,6 +311,7 @@ public class AboutFragmentSelecter extends Fragment {
         //danDetails.setTypeface(retroFont);
         //TextView jackDetails = (TextView) view.findViewById(R.id.jack);
         //jackDetails.setTypeface(retroFont);
+         */
     }
 
 
