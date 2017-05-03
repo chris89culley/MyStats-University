a53c7beddb4e74f9a1943adcefac2a8e7fe236aa
Jack <jacklundy@hotmail.co.uk>
2017-05-03 12:56:52 +0100

    Added : .new coloyrs and sizing for icons

--- app/src/main/java/FragmentSelectors/UcasTipsFragmentSelecter.java
+++ app/src/main/java/FragmentSelectors/UcasTipsFragmentSelecter.java
@@ -135,9 +135,9 @@ public class UcasTipsFragmentSelecter extends Fragment {
         tips.add(new TipEntry(R.string.ucas_tips_hobby_title, R.string.ucas_tips_hobby,  R.drawable.bowling));
         tips.add(new TipEntry(R.string.ucas_tips_entry_requirments_title, R.string.ucas_tips_entry_requirments, R.drawable.microscope));
         tips.add(new TipEntry(R.string.ucas_tips_location_title, R.string.ucas_tips_location, R.drawable.house));
-        tips.add(new TipEntry(R.string.ucas_tips_goals_title, R.string.ucas_tips_goals, R.drawable.targets_icon));
-        tips.add(new TipEntry(R.string.ucas_tips_research_title, R.string.ucas_tips_research,  R.drawable.goals_icon));
-        tips.add(new TipEntry(R.string.ucas_tips_talk_title, R.string.ucas_tips_talk, R.drawable.chat_icon));
+        tips.add(new TipEntry(R.string.ucas_tips_goals_title, R.string.ucas_tips_goals, R.drawable.goals_icon4));
+        tips.add(new TipEntry(R.string.ucas_tips_research_title, R.string.ucas_tips_research,  R.drawable.targets_icon2));
+        tips.add(new TipEntry(R.string.ucas_tips_talk_title, R.string.ucas_tips_talk, R.drawable.chat_icon2));
 
         if(!tips.isEmpty()){
             adapter = new TipWithIconListAdapter(getActivity(), R.layout.tip_list_row, R.layout.tip_list_header, tips);
