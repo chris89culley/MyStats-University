292c205e12c8b36562e375b109eda7081c3db51f
Chris <iamchris88@live.co.uk>
2017-05-06 07:23:29 +0100

    Added a few comments

--- app/src/main/java/com/example/chris/mystats_univeristy/About.java
+++ app/src/main/java/com/example/chris/mystats_univeristy/About.java
@@ -4,6 +4,9 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
 import android.view.Window;
 
 import com.example.chris.mystats_univeristy.MenuViewActivity;
@@ -21,6 +24,7 @@ public class About extends MenuViewActivity {
 
         ViewPager vp = (ViewPager) findViewById(R.id.aboutViewPager);
         vp.setAdapter(new AboutPageAdapter(getSupportFragmentManager(), this));
+
 
 
         tb = (TabLayout) findViewById(R.id.aboutTabLayout);
