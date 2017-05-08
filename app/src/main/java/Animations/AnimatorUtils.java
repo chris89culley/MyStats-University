46ec61e56ca7464cd7f9077826320571fd90d72d
Chris <iamchris88@live.co.uk>
2017-05-08 08:20:03 +0100

    Refactor

--- app/src/main/java/Animations/AnimatorUtils.java
+++ app/src/main/java/Animations/AnimatorUtils.java
@@ -2,8 +2,9 @@ package Animations;
 
 
 /**
+ *
  * This class is directly taken from https://github.com/ogaclejapan/ArcLayout/blob/master/demo/src/main/java/com/ogaclejapan/arclayout/demo/AnimatorUtils.java
- * and is used in the arch animations
+ * and is used to create animations with an ObjectAnimator which operates on several different properties in parallel
  *
  */
 
@@ -11,32 +12,53 @@ import android.animation.PropertyValuesHolder;
 
 public class AnimatorUtils {
 
-    public static final String ROTATION = "rotation";
-    public static final String SCALE_X = "scaleX";
-    public static final String SCALE_Y = "scaleY";
-    public static final String TRANSLATION_X = "translationX";
-    public static final String TRANSLATION_Y = "translationY";
+    private static final String ROTATION = "rotation";
+    private static final String SCALE_X = "scaleX";
+    private static final String SCALE_Y = "scaleY";
+    private static final String TRANSLATION_X = "translationX";
+    private static final String TRANSLATION_Y = "translationY";
 
-    private AnimatorUtils() {
-        //No instances.
-    }
-
+    /**
+     * Gets the values required for a rotation
+     * @param values - The rotation values
+     * @return - The values holder for a rotation
+     */
     public static PropertyValuesHolder rotation(float... values) {
         return PropertyValuesHolder.ofFloat(ROTATION, values);
     }
 
+    /**
+     * Gets the values required for an x translation
+     * @param values - The values by which x is translated
+     * @return - The values holder for an x translation
+     */
     public static PropertyValuesHolder translationX(float... values) {
         return PropertyValuesHolder.ofFloat(TRANSLATION_X, values);
     }
 
+    /**
+     * Gets the values required for an y translation
+     * @param values - The values by which y is translated
+     * @return - The values holder for an y translation
+     */
     public static PropertyValuesHolder translationY(float... values) {
         return PropertyValuesHolder.ofFloat(TRANSLATION_Y, values);
     }
 
+    /**
+     * Gets the values required for an x scale
+     * @param values - The values by which x is scaled
+     * @return - The values holder for an x scale
+     */
     public static PropertyValuesHolder scaleX(float... values) {
         return PropertyValuesHolder.ofFloat(SCALE_X, values);
     }
 
+    /**
+     * Gets the values required for a y scale
+     * @param values - The values by which y is scaled
+     * @return - The values holder for a y scale
+     */
     public static PropertyValuesHolder scaleY(float... values) {
         return PropertyValuesHolder.ofFloat(SCALE_Y, values);
     }
